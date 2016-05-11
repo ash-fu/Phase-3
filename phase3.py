2c059fa1a00f2540996ec9579c707a6d99979103
Quoc Lieu <qlieu@student.unimelb.edu.au>
2016-05-11 16:24:36 +1000

    set up workspace

---
+++ phase3.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route("/")
+def index():
+    return render_template("frontpage.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=2525)
