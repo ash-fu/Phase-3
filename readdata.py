f19623380c2c224b3085dbda56fbea09fb9795d2
Jennifer Fong <jenniferfong@Jennifers-MacBook-Air.local>
2016-05-14 15:51:01 +1000

    changed the csv with bins

--- readdata.py
+++ readdata.py
@@ -1,9 +1,21 @@
-
-
 import csv
 
 content = open("data_2012.csv")
 reader = csv.reader(content)
 headers = reader.next()
+data = zip(*reader)
 
+categorical_headers= []
+numerical_headers= []
+
+count= 0
+for column in data:
+    if column[1].isdigit():
+        numerical_headers.append(headers[count])
+    else:
+        categorical_headers.append(headers[count])
+    count +=1 
+    
 print headers
+print categorical_headers
+print numerical_headers
