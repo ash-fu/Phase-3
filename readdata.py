8bc7319c401154403ccf1bb6f1be2c0b37c6e42d
Jennifer Fong <jenniferfong@Jennifers-MacBook-Air.local>
2016-05-22 17:03:04 +1000

    observations and csv

--- readdata.py
+++ readdata.py
@@ -14,16 +14,28 @@ def getheaders(filename):
             numerical_headers.append(headers[count])
         else:
             categorical_headers.append(headers[count])
-        count +=1    
+        count +=1
     header_type.append(numerical_headers)
     header_type.append(categorical_headers)
     return header_type
 
 def num_header(filename):
     headers = getheaders(filename)
-    return headers[1]
+    return headers[0]
 
 def cate_header(filename):
     headers = getheaders(filename)
-    return headers[0]
+    return headers[1]
 
+def gender(filename):
+    content = open(filename)
+    reader = csv.reader(content)
+    headers = reader.next()
+    male = []
+    female = []
+    for row in reader:
+        if row[9] == "Male":
+            male.append(row[10])
+        else:
+            female.append(row[10])
+    return (male,female)
