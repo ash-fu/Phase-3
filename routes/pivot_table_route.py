307464e41cd61b9a87b64cf98618e82364797f93
Quoc Lieu <qlieu@student.unimelb.edu.au>
2016-05-23 04:57:36 +1000

    pivot table

--- routes/pivot_table_route.py
+++ routes/pivot_table_route.py
@@ -1,26 +1,45 @@
-from flask import render_template
+from flask import render_template, request
 from phase3 import app
 import pandas as pd
 import numpy as np
 from processPT import *
 
-#server/pviot_table
-@app.route("/pivot_table")
+#server/pivot_table
+@app.route("/pivot_table",methods=['POST', 'GET'])
 def pivot_table():
-	filteredData = filterData(element,userInput,filterMethod)
-	table = constructPT(data,rows,columns,values,agg)
+	title = "Pivot Table"
+	template_vars = {
+		"title": title
+	}
+
+	filterCategory = request.form['Filter Category']
+	filterMethod = request.form['Filter Method']
+	filterValue = request.form['Filter Value']
+	row = request.form['Row']
+	column = request.form['Column']
+	agg = request.form['agg']
+	values = request.form['Values']
+	
+	#filteredData = filterData(filterCategory,filterValue,filterMethod)
+	#table = constructPT(filteredData,row,column,values,agg)
 	with open('templates/pivot_table.html' , 'w') as html:
-		html.write('''
-			{% extends "base.html" %}
-			{% block content %}
-			''')
+		# html.write('''
+		# 	{% extends "base.html" %}
+		# 	{% block content %}
+		# 	'''+table.to_html())
 
 
 
+		# html.write('''
+		# 	{% endblock %}
+		# ''')
+		body = '''Filter Method: %s <br>
+		Filter Category: %s <br>
+		Filter Value: %s <br>
+		Aggregation: %s <br>
 
+		'''
+		body = body % (filterCategory,filterMethod,filterValue,agg)
+		html.write(body)
 
-
-		html.write('''
-			{% endblock %}
-		''')
-	return render_template("pivot_table.html")
+	return render_template("pivot_table.html",vars=template_vars)
